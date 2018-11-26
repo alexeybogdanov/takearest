5dccfe355f5f7b0cdd737f8ee9775f536ae5bd87
Alex B <aleksei.n.bogdanov@gmail.com>
2018-11-27 00:24:02 +0300

    Initial commit

---
+++ src/main/java/com/example/takearest/service/EmployeeServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.takearest.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.takearest.entity.Employee;
+import com.example.takearest.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public void setEmployeeRepository(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> retrieveEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees;
+    }
+
+    public Employee getEmployee(Long employeeId) {
+        Optional<Employee> optEmp = employeeRepository.findById(employeeId);
+        return optEmp.get();
+    }
+
+    public void saveEmployee(Employee employee){
+        employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long employeeId){
+        employeeRepository.deleteById(employeeId);
+    }
+
+    public void updateEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+}
