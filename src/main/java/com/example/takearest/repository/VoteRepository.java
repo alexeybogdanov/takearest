2f96f1265c8d35b6602a200b802c5061bc9c4745
Alex B <aleksei.n.bogdanov@gmail.com>
2019-01-19 12:57:47 +0300

    remove @Transactional to repository

--- src/main/java/com/example/takearest/repository/VoteRepository.java
+++ src/main/java/com/example/takearest/repository/VoteRepository.java
@@ -4,13 +4,11 @@ import com.example.takearest.entity.User;
 import com.example.takearest.entity.Vote;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 @Repository
-@Transactional(readOnly = true)
 public interface VoteRepository extends CrudRepository<Vote, Long> {
 
     /**
