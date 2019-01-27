619d0719304b48a286c309175532ca6283eb82f0
Alex B <aleksei.n.bogdanov@gmail.com>
2019-01-27 22:55:21 +0300

    fixed imports

--- src/main/java/com/example/takearest/entity/Restaurant.java
+++ src/main/java/com/example/takearest/entity/Restaurant.java
@@ -1,6 +1,5 @@
 package com.example.takearest.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.AllArgsConstructor;
 import lombok.Data;
